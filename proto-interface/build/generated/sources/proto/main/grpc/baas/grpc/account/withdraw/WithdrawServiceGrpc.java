5cc637840d3f6c005588f9afd95edddb5ec757e0
SYJ_MSI <tom990422@gmail.com>
2025-05-07 16:58:49 +0900

    fix: remove javax

--- proto-interface/build/generated/sources/proto/main/grpc/baas/grpc/account/withdraw/WithdrawServiceGrpc.java
+++ proto-interface/build/generated/sources/proto/main/grpc/baas/grpc/account/withdraw/WithdrawServiceGrpc.java
@@ -4,9 +4,6 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 
 /**
  */
-@javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.70.0)",
-    comments = "Source: withdraw.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class WithdrawServiceGrpc {
 
